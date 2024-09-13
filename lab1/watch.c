e02adab95855c30e714cc407c73199ebfbcaafd6
Kam-Ya <kamranyaghoubian@gmail.com>
2024-09-13 15:32:30 -0400

    added comments

--- lab1/watch.c
+++ lab1/watch.c
@@ -6,7 +6,11 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+
+// this program is meant to loop infinitely, so to end it kill the process (ctrl + c)
 int main(void) {
+
+    // inifite loop checking for any libre office instance and immediately kills it (repeating every second)
     while (1) {
         system("kill $(ps aux | grep libreoffice | awk '{print $2}')");
         sleep(1);
