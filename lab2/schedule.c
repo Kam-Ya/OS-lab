ce8e45aa5bffee7012750a2c3ccb2eb5dd1007d7
Kam-Ya <kamranyaghoubian@gmail.com>
2024-10-08 11:30:07 -0400

    FEAT: added build function for DLLS

--- lab2/schedule.c
+++ lab2/schedule.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 struct node { // contains node information
-    char key;
+    char* key;
     unsigned long priority;
 };
 
@@ -13,31 +13,64 @@ struct element { // contains node and pointers to previous and next nodes
 };
 
 struct DLLS { // double linked list
-    struct Element * start;
+    struct element * start;
 };
 
 struct queue {
-    struct DLLS list;
+    struct DLLS * list;
     unsigned long elements; // number of entities in queue
 };
 
-void addElement(struct queue * L, struct node Node);
+void enqueue(struct queue * L, struct node Node);
 void removeElement (struct queue * L);
-void runScheduled(struct queue * L);
-
+void dequeue(struct queue * L);
+struct queue * build();
 
 int main (void) {
 
 }
 
-void addElement(struct queue * L, struct node Node) {
+void enqueue(struct queue * L, struct node Node) {
 
 }
 
-void runScheduled(struct queue * L) {
+void dequeue(struct queue * L) {
 
 }
 
 void removeElement (struct queue * L) {
     
+}
+
+struct queue * build() { // build the structure of the queue, mostly memory assignment
+    struct queue * Q = NULL;
+    Q = (struct queue *) malloc(sizeof(struct queue));
+
+    if (Q) {
+        if (Q -> list = malloc(sizeof(struct DLLS))) {
+            Q -> list -> start = malloc(sizeof(struct element));
+        } else {
+            printf("Error in assigning memory to list\n");
+            return 0;
+        }
+
+        // since this is a double linked list im assigning the start node to point to itself both forwards and in reverse
+        if (Q -> list -> start) { 
+            Q -> list -> start -> next = Q -> list -> start;
+            Q -> list -> start -> prev = Q -> list -> start;
+
+            // start node doesnt need any information its never read besides its next and prev pointers
+            Q -> list -> start -> info.key = NULL;
+            Q -> list -> start -> info.priority = NULL;
+        } else {
+            printf("Error in assigning memory to start node");
+            return 0;
+        }
+        
+    } else {
+        printf("Error in assigning memory for a the Queue");
+        return 0;
+    }
+
+    return Q;
 }
