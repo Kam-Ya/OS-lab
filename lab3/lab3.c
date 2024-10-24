35ee7e6e59349e53d0c121ce1bf1e879aa9f6136
Kam-Ya <kamranyaghoubian@gmail.com>
2024-10-23 22:43:27 -0400

    first working version, will probably try to improve if i have time

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,6 +1,6 @@
 #include <stdio.h> 
 #include <stdbool.h>
-#include <math.h>
+#include <unistd.h>
 
 int main(void) {
 
@@ -26,7 +26,32 @@ int main(void) {
         return 1;
     }
 
-
     bool unusedBlocks[blocks]; 
+    bool more = false;
+
+    // all these ifs are redudent and need to be better optimised
+    while (files != 0) {
+
+        for (int i = 0; i < blocks; i++) { // release all memory as if processes finishes running
+            unusedBlocks[i] = true;
+            if (files != 0) {
+                unusedBlocks[i] = false;
+                files--;
+            }
+        }
+        
+        if (files != 0) {
+            more = true; 
+            printf("All memory blocks in use, waiting for free memory\n");
+        }
+        sleep(1);
+    }
+
+    if (more == false) { // output for the lab
+        printf("All files fit into memory blocks on without having to wait!\n");
+    } else {
+        printf("all processes have been run!\n");
+    }
+    
     return 0;
 }
