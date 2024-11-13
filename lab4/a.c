66529c818b37b9237a14293c445a4ccc2319dd26
Kam-Ya <kamranyaghoubian@gmail.com>
2024-11-13 16:14:44 -0500

    simplest possible implementaiton of codeing problem a

--- lab4/a.c
+++ lab4/a.c
@@ -1,3 +1,16 @@
+
 int main(void) {
+    int index;
+    int offset;
+    int pageTable[8] = {1, 5, 2, 9, 2, 7, 11, 4};
+
+    printf("Enter a page number (0-7): ");
+    scanf("%d", &index);
+    printf("Enter offset: ");
+    scanf("%d", &offset);
+
+    printf("Frame number is: %d\n", pageTable[index]);
+    printf("Offset is: %d\n", offset);
+
     return 0;
 }
