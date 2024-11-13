d16a960b2fe0ada298b41d4638d2d74fcee0fdac
Kam-Ya <kamranyaghoubian@gmail.com>
2024-11-13 16:20:07 -0500

    added structures for a DLL

--- lab4/b.c
+++ lab4/b.c
@@ -1,3 +1,25 @@
+struct node { // contains node information
+    char* key;
+    int priority;
+    int time;
+};
+
+struct element { // contains node and pointers to previous and next nodes
+    struct node info;
+    struct element * next, * prev;
+};
+
+struct DLLS { // double linked list
+    struct element * start;
+};
+
+struct queue {
+    struct DLLS * list;
+    unsigned long elements; // number of entities in queue
+};
+
+
 int main(void) {
+
     return 0;
 }
