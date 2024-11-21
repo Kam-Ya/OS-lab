afacf379297f50437c3167fd10d53c21d5c3c415
Kam-Ya <kamranyaghoubian@gmail.com>
2024-11-21 11:19:41 -0500

    added basic structure for main program

--- lab4/b.c
+++ lab4/b.c
@@ -1,25 +1,51 @@
-struct node { // contains node information
-    char* key;
-    int priority;
-    int time;
-};
-
-struct element { // contains node and pointers to previous and next nodes
-    struct node info;
-    struct element * next, * prev;
-};
-
-struct DLLS { // double linked list
-    struct element * start;
-};
-
-struct queue {
-    struct DLLS * list;
-    unsigned long elements; // number of entities in queue
-};
+#include <stdio.h>
+#include <ctype.h>
+#include "DLL.h"
 
 
 int main(void) {
+    struct queue * Q = build();
+    int number;
+    struct node current;
+    char sequence [50];
+    int hits = 0;
+    int faults = 0;
+
+    // collect input from user for later calculations;
+    printf("How many frams: ");
+    scanf("%d", &number);
+
+    printf("Enter page (single digit) sequence seperated by spaces: ");
+    fgets(sequence, sizeof(sequence), stdin);
+
+    for (int i = 0; i < sizeof(sequence); i++) {
+        // checks if input is valid and if current loop is workable
+        if (sequence[i] == '\0') {
+            break;
+        } else if (sequence[i] == ' ') {
+            continue;
+        } else if (isdigit(sequence[i]) == 0) {
+            printf("invalid input\n");
+            break;
+        }
+
+        current.key = (int) sequence;
+
+        // finds if current page already exists adnd moves it to the front
+        if (search(Q, current.key) != NULL) {
+            hits++;
+            dequeueFound(Q, current.key);
+            enqueue(Q, current);
+        } else { // otherwise adds element to front of queue, removing last element if necesarry
+            faults++;
+            if (Q->elements == number) {
+                dequeue(Q);
+                enqueue(Q, current);
+            } else {
+                enqueue(Q, current);
+            }
+        }
+    }
 
     return 0;
 }
